473208f908aba1218dbd1399d215ec785ade2cbb
cjtorralba <cjtorralba@gmail.com>
2021-01-31 18:25:23 -0800

    Finalized version

--- A2/setBits.c
+++ A2/setBits.c
@@ -9,9 +9,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//defining size for each of use
+//defining size for ease of use
 #define SHORT_SIZE sizeof(unsigned short) * 8
 
+
+
+//populating the array with values from argv, EXCLUDING "./a.out"
 void populateArray(int* array, char** argv, int argc)
 {
 	for(int i = 0; i < argc; i++)
@@ -20,69 +23,76 @@ void populateArray(int* array, char** argv, int argc)
 		{
 			int argument = (int) strtol(argv[i+1], (char**)NULL, 10);
 			if(argument < 0 || argument > 15)
+			{
+				printf("Please run again with valid command line inputs");
 				exit(EXIT_FAILURE);
+			}
 			else
 				array[i] = argument;
 		}
-
 	}
 }	
 
+
+//setting the bits in a short, taking an array of positions 
+unsigned short setShort(int* positions, int arrSize)
+{
+	unsigned short toReturn = 0; //value we will be returning
+	for(int i = 0; i < arrSize; ++i)
+	{
+		toReturn |= 1 << positions[i];	//setting bit in positions from positions array
+//		printf("Setting bit in position: %d\n", positions[i]);
+	}
+	return toReturn;
+}
+
+
+//prints the number as a binary
+void printBits(unsigned short toPrint)
+{
+	for(int i = SHORT_SIZE - 1; i >= 0; --i)
+	{
+		printf("%d", (toPrint >> i) & 1);
+		if(i % 4 == 0)
+			printf(" ");
+	}
+}
+
+
+
 int main(int argc, char** argv)
 {
-
-	int arrSize = argc - 1;	//size of array, how many arguments - 1 for the a.out value
-	unsigned short myShort; //short we are going to be flipping the bits in
-
-//	int bitPosition;	//the bit position we are going to be manipulating
-
-	if(arrSize <= 0)	//if there were no arguments passed in
+	if(argc <= 1 || argc > 17)	//if there were no arguments passed in
 	{
 		printf("Please run again with proper command line arguments");
 		exit(EXIT_FAILURE);
 	}
 
-	myShort = 0;
+	//short we are going to be setting bits in
+	unsigned short myShort = 0;
 
-	int positions[arrSize];	//array of positions
+	//size of array holding bit positions we will be flipping
+	int arrSize = argc - 1;
 
+	//array holding bit positions
+	int positions[arrSize];
+	//populating the array with variables from argv
 	populateArray(positions, argv, argc);
 
-	//setting the bit
-	for(int i = 0; i < arrSize; ++i)
-		myShort = myShort | (1 << positions[i]);
-
-	//telling the user which bits were set
-	printf("You flipped the bits in positions: ");
+	//setting the short, modifying the bits in it
+	myShort = setShort(positions, arrSize);
+	
+	//printing the position of the bits that are set
+	printf("You set the bits in positions: ");
 	for(int i = 0; i < arrSize; ++i)
 		printf("%d, ", positions[i]);
 	printf("\n\n");
 
-	int values[SHORT_SIZE] = {0};
+	//telling user what the integer value of the short is after setting the bits
+	printf("The integer value of the unsigned short is: %d\n", myShort);
 
-	for(int i = 0; i < SHORT_SIZE; ++i)
-	{
-		values[i] = (myShort >> i) & 1;	
-	}
-
-	for(int i = SHORT_SIZE-1; i > 0; --i)
-	{
-		printf("Values[%d]: %d\n", i, values[i]);
-	}
-	
-
-	//printing the unsigned short as a binary number
-	for(int i = 0; i < SHORT_SIZE; ++i)
-	{
-		if(i == 0);//do nothing, we dont want to add a space at the very start
-
-		else if(i % 4 == 0)	//adding space after each 4 numbers for readability
-			printf(" ");
-
-		printf("%d", (myShort >> i) & 1);
-	}
-
-	printf("\nThe value of this binary number is: %d\n", myShort);
+	//printing the binary version of the short
+	printBits(myShort);
 
 	exit(EXIT_SUCCESS);
 }
